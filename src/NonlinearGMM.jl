daf658879a4c75632dd2b7540d479b523e008619
Christoph Lenovo <christophhedtrich@gmail.com>
2018-03-17 19:59:42 +0100

    add sur test. works. but need to check variance covariance matrix

--- src/NonlinearGMM.jl
+++ src/NonlinearGMM.jl
@@ -1,5 +1,6 @@
 module NonlinearGMM
     using Optim
+    using ForwardDiff
     """
     Nonlinear GMM
     """
@@ -8,20 +9,28 @@ module NonlinearGMM
         opt = nlgmm_opt(gfun, xdata, ydata, W, N, Nm, Nb; guess = guess, autodiff = autodiff)
         bgmm = opt.minimizer
         # calculcate variance covariance matrix
-
+        V = nlgmm_covar(bgmm, gfun, xdata, zdata, ydata, W, N, Nm, Nb)
+        return bgmm, V
     end
 
     """
     Parameter estimation
     """
-    function nlgmm_opt(gfun, xdata, ydata, W, N, Nm, Nb; guess = ones(Nb), autodiff = :forward)
+    function nlgmm_opt(gfun, xdata, ydata, W, N, Nm, Nb; guess = ones(Nb), autodiff = :forward, maxit = 1000)
+        f(x) = J(x, gfun, xdata, ydata, W, N, Nm, Nb)
         if autodiff == :forward
-            f(x) = J(x, gfun, xdata, ydata, W, N, Nm, Nb)
-            obj = TwiceDifferentiable(f, guess; autodiff = :forward)
+            obj = OnceDifferentiable(f, guess; autodiff = :forward)
+            println("J obj prepared. Start Minimization")
+            res = optimize(obj, guess, method = LBFGS(), show_trace = true, iterations = maxit, show_every = 10)
+
+        elseif autodiff == :false
+            println("J obj prepared. Start Minimization")
+            # need to add gradient fun
+            res = optimize(f, guess, method = LBFGS(), show_trace=true, iterations = maxit, show_every = 10)
         end
-        res = optimize(obj, guess, Newton())
         return res
     end
+
 
     function J(b, gfun, xdata, ydata, W, N, Nm, Nb)
         gvec = zeros(eltype(b),(N,Nm))
@@ -35,9 +44,24 @@ module NonlinearGMM
     """
     Covariance Matrix given data, parameter estimates and weights
     """
-    function nlgmm_covar(bhat, gfun, xdata, zdata, ydata, W, N, Nm, Nb)
+    function nlgmm_covar(bhat, gfun, xdata, ydata, W, N, Nm, Nb)
+        gvec = zeros(eltype(bhat),(N,Nm))
+        for i = 1:N
+            gvec[i,:] = gfun(bhat, xdata[i,:], ydata[i,:])
+        end
+        gbar = mean(gvec,1)[:]
         # construc Ω
+        Ω = zeros(eltype(bhat),(Nm,Nm))
+        for i = 1:N
+            Ω += 1/N * (gvec[i,:]-gbar) *(gvec[i,:]-gbar)'
+        end
         # contstruct Q
+        Q = zeros(eltype(bhat),(Nm, Nb))
+        for i = 1:N
+            Q += 1/N * ForwardDiff.jacobian(par -> gfun(par, xdata[i,:], ydata[i,:]), bhat)' # check transpose
+        end
+        # check division by N
+        return inv(Q'*W*Q)*(Q'*W*Ω*W*Q)*inv(Q'*W*Q)
     end
 
 end # module
--- test/surtest.jl
+++ test/surtest.jl
@@ -1,11 +1,15 @@
+using NonlinearGMM
+using Base.Test
+
+
 # Seemingly Unrelated Regressions
 # setup multiple equations with same covariates
-N = 10000
-nx = 1
-neq = 2
+const N = 500
+const nx = 2
+const neq = 2
 βh = rand((nx+1, neq))*10
-X = rand((N, nx+1))
-y = X*βb .+ randn((N, neq))
+X = hcat(ones(N), rand((N, nx)))
+y = X*βh .+ randn((N, neq))
 Xbar = kron(eye(neq), X)
 β = reshape(βh, (nx+1)*neq, 1)
 ystack = reshape(y, N*neq, 1)
@@ -15,16 +19,58 @@ bols_stack = (Xbar'Xbar)\(Xbar'*ystack)
 
 # Σ estimate
 uhat = y .- X*bols
-Σhat = zeros((nx+1, nx+1))
+Σhat = zeros((neq, neq))
 for i = 1:N
     Σhat += 1/N * uhat[i, :] * uhat[i,:]'
 end
+# now redo
 # calculate V based on OLS, Hansen stacks the equations
 # define Xbar as he does and stack β
 # eq (10.17) in Bruce Hansens Econometrics book
-# Σhatinv = inv(Σhat)
-# XΣinv = zeros((nx+1, nx+1))
-# for i = 1:N
-#     XΣinv +=  1/N * X[i, :]
-# end
+Xi = zeros((neq,(nx+1)*neq))
+Σhatinv = inv(Σhat)
+XΣinvX = zeros(((nx+1)*neq, (nx+1)*neq))
+XΣinvy = zeros(((nx+1)*neq, 1))
+
+for i = 1:N
+    Xi[1,1:nx+1] = X[i,:]
+    Xi[2,nx+2:end] = X[i,:]
+    XΣinvX +=  1/N * Xi' * Σhatinv * Xi
+    XΣinvy += 1/N * Xi' * Σhatinv * y[i,:]
+end
+bsur = XΣinvX\XΣinvy
+Vbsur = inv(N*XΣinvX) # should the N b here?
+
 # GMM variant
+# define moment function for observation i
+# super slow!
+function gsur(par, xdata, ydata)
+    b = par[1:(nx+1)*neq]
+    Σ = reshape(par[(nx+1)*neq+1:end], neq, neq)
+    Σinv = inv(Σ)
+    # first stack the parameters of the linear model and then Σ
+    Xi = zeros((neq,(nx+1)*neq))
+    Xi[1,1:nx+1] = xdata
+    Xi[2,nx+2:end] = xdata
+    u = (ydata-Xi*b)
+    g1 = Xi'*Σinv * u
+    g2 = (Σ - u*u')[:]
+    return vcat(g1, g2)
+end
+
+btest = vcat(bsur[:], Σhat[:])
+Nm = length(btest)
+Nb = length(btest)
+W = eye(length(btest))
+
+@test isapprox(0.,NonlinearGMM.J(btest, gsur, X, y, W, N, Nm, Nb), atol=sqrt(eps()))
+
+# now estimate gmm
+bguess =vcat(zeros(bsur[:]), eye(neq)[:])
+
+res = NonlinearGMM.nlgmm_opt(gsur, X, y, W, N, Nm, Nb; guess = bguess, autodiff = :forward, maxit = 5000)
+
+bgmm = res.minimizer[1:(nx+1)*neq]
+Σgmm = reshape(res.minimizer[(nx+1)*neq+1:end], neq, neq)
+# add Variance Covariance matrix GMM
+V = NonlinearGMM.nlgmm_covar(res.minimizer, gsur, X, y, W, N, Nm, Nb)
