c825c387c381a2c5dc394022b596929fb27f2215
Christoph Lenovo <christophhedtrich@gmail.com>
2018-03-18 21:52:03 +0100

    correct sur test to eliminate redundant variance matrix element

--- test/surtest.jl
+++ test/surtest.jl
@@ -44,10 +44,21 @@ Vbsur = inv(N*XΣinvX) # should the N b here?
 
 # GMM variant
 # define moment function for observation i
-# super slow!
-function gsur(par, xdata, ydata)
+function unpack_par(par)
     b = par[1:(nx+1)*neq]
-    Σ = reshape(par[(nx+1)*neq+1:end], neq, neq)
+    Σ = diagm(par[(nx+1)*neq+1:(nx+1)*neq+neq])
+    stop = (nx+1)*neq+neq
+    start = (nx+1)*neq+neq
+    for ieq = 2:neq
+        start = stop + 1
+        stop = start + (neq -ieq)
+        Σ += diagm(par[start:stop] , (ieq-1)) + diagm(par[start:stop] , -(ieq-1))
+    end
+    return b, Σ
+end
+function gsur(par, xdata, ydata)
+    b, Σ = unpack_par(par)
+    #Σ = reshape(par[(nx+1)*neq+1:end], neq, neq)
     Σinv = inv(Σ)
     # first stack the parameters of the linear model and then Σ
     Xi = zeros((neq,(nx+1)*neq))
@@ -55,11 +66,22 @@ function gsur(par, xdata, ydata)
     Xi[2,nx+2:end] = xdata
     u = (ydata-Xi*b)
     g1 = Xi'*Σinv * u
-    g2 = (Σ - u*u')[:]
+    G2 = (Σ - u*u')
+    g2 = zeros(eltype(par),Int(neq^2 - (neq*(neq-1))/2))
+    start = 1
+    stop = 1
+    for d = 1:neq
+        stop = start + neq - d
+        g2[start:stop] = diag(G2, d-1)
+        start = stop +1
+    end
+
     return vcat(g1, g2)
 end
 
-btest = vcat(bsur[:], Σhat[:])
+btest = vcat(bsur[:], diag(Σhat),  diag(Σhat, 1))
+
+
 Nm = length(btest)
 Nb = length(btest)
 W = eye(length(btest))
@@ -67,10 +89,9 @@ W = eye(length(btest))
 @test isapprox(0.,NonlinearGMM.J(btest, gsur, X, y, W, N, Nm, Nb), atol=sqrt(eps()))
 
 # now estimate gmm
-bhat, V, Q, Ω = nlgmm(gsur, X, y, W, N, Nm, Nb; guess = vcat(zeros((nx+1)*neq), eye(neq)[:]), autodiff = :forward, maxit = 5000)
+bhat, V, Q, Ω = nlgmm(gsur, X, y, W, N, Nm, Nb; guess = vcat(ones(bsur), ones(neq), zeros(Int(neq*(neq-1)/2))), autodiff = :forward, maxit = 5000)
 
-bgmm = bhat[1:(nx+1)*neq]
-Σgmm = reshape(bhat[(nx+1)*neq+1:end], neq, neq)
+bgmm, Σgmm = unpack_par(bhat)
 
 # add Variance Covariance matrix GMM
 # V is wrong need to divide by N! Where is it missing in my code?
@@ -86,4 +107,5 @@ for it = 1:10
     W[:,:] = inv(Ω)
 end
 bgmm_it = bhat[1:(nx+1)*neq]
+
 @test isapprox(bgmm_it, bols_stack, atol=sqrt(eps()))
